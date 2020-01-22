39b637558ee86b08a4cf8b8f683dfe364231751e
Henrik <henrik.mosgaard.jensen@gmail.com>
2020-01-22 01:53:22 +0100

    Add fake swagger.json endpoint using webapi controller

---
+++ MergeOpenApi.Ui/Controllers/ApiController.cs
@@ -0,0 +1,28 @@
+﻿using MergeOpenApi.Ui.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MergeOpenApi.Ui.Controllers
+{
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly IGetSchemaCached _getSchemaCached;
+
+        public ApiController(IGetSchemaCached getSchemaCached)
+        {
+            _getSchemaCached = getSchemaCached;
+        }
+        
+        [HttpGet("/swagger.json")]
+        public ActionResult Get()
+        {
+            var json = _getSchemaCached.Execute();
+            if (json == null)
+            {
+                return NotFound();
+            }
+            
+            return Content(json, "application/json");
+        }
+    }
+}
