a546e2c14d813257cef61309ca5c689070fd36cc
HenrikDK <commander.john.crichton@gmail.com>
2020-01-22 01:51:39 +0100

    add infrastructure

---
+++ MergeOpenApi.Ui/Infrastructure/ApiRegistry.cs
@@ -0,0 +1,21 @@
+using Lamar;
+
+namespace MergeOpenApi.Ui.Infrastructure
+{
+    public class ApiRegistry : ServiceRegistry
+    {
+        public ApiRegistry()
+        {
+            Scan(x =>
+            {
+                x.AssemblyContainingType<Program>();
+                
+                x.WithDefaultConventions();
+
+                x.LookForRegistries();
+                
+                x.ExcludeType<ApiRegistry>();
+            });
+        }
+    }
+}
