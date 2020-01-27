28009b2425706ba0b3d8fe6c735650d3dad39046
Henrik <henrik.mosgaard.jensen@gmail.com>
2020-01-27 15:20:26 +0100

    add business logic for fetching and merging open api schemas

---
+++ MergeOpenApi/Merge/FetchServiceDefinitions.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Flurl;
+using Flurl.Http;
+using MergeOpenApi.Model;
+using MergeOpenApi.Model.Cache;
+using MergeOpenApi.Model.Commands;
+using MergeOpenApi.Model.Enums;
+using MoreLinq;
+using MoreLinq.Extensions;
+
+namespace MergeOpenApi.Merge
+{
+    public interface IFetchServiceDefinitions
+    {
+        void Execute(IList<ServiceDefinition> services);
+    }
+    
+    public class FetchServiceDefinitions : IFetchServiceDefinitions
+    {
+        private readonly IUpdateServiceJson _updateServiceJson;
+        private readonly IGetConfigurationCached _getConfigurationCached;
+        private readonly IUpdateServiceStatus _updateServiceStatus;
+
+        public FetchServiceDefinitions(IUpdateServiceJson updateServiceJson,
+            IGetConfigurationCached getConfigurationCached,
+            IUpdateServiceStatus updateServiceStatus)
+        {
+            _updateServiceJson = updateServiceJson;
+            _getConfigurationCached = getConfigurationCached;
+            _updateServiceStatus = updateServiceStatus;
+        }
+        
+        public void Execute(IList<ServiceDefinition> services)
+        {
+            var exceededRetry = services.Where(x => x.Retry >= 10).ToList();
+            if (exceededRetry.Any())
+            {
+                exceededRetry.ForEach(x => x.Status = ServiceStatus.Disabled);
+                _updateServiceStatus.Execute(exceededRetry);
+            }
+            
+            var deployed = services.Except(exceededRetry).ToList();
+            deployed.ForEach(FetchServiceDefinition);
+
+            var fetched = deployed.Where(x => x.Status == ServiceStatus.Fetched).ToList();
+            var notFetched = deployed.Where(x => x.Status != ServiceStatus.Fetched).ToList();
+
+            if (fetched.Any())
+            {
+                _updateServiceJson.Execute(fetched);
+            }
+
+            if (notFetched.Any())
+            {
+                _updateServiceStatus.Execute(notFetched);
+            }
+        }
+
+        private void FetchServiceDefinition(ServiceDefinition service)
+        {
+            var configuration = _getConfigurationCached.Execute();
+            var segment = "/swagger.json";
+            if (!string.IsNullOrEmpty(configuration.JsonEndpoint) && configuration.JsonEndpoint.Contains(".json"))
+            {
+                segment = configuration.JsonEndpoint;
+            }
+            
+            var urls = service.ServiceUrls.Split(',');
+
+            foreach (var url in urls)
+            {
+                try
+                {          
+                    var json = url
+                        .AppendPathSegment(segment)
+                        .GetStringAsync().Result;
+                
+                    if (json.Length > 0)
+                    {
+                        service.JsonData = json;
+                        service.Status = ServiceStatus.Fetched;
+                    }
+                }
+                catch (Exception e){}
+
+                if (service.Status == ServiceStatus.Fetched)
+                {
+                    return;
+                }
+            }
+
+            service.JsonData = null;
+            service.Retry++;
+        }
+    }
+}
---
+++ MergeOpenApi/Merge/MergeOpenApiSchemas.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using MergeOpenApi.Model;
+using MergeOpenApi.Model.Cache;
+using MergeOpenApi.Model.Commands;
+using MergeOpenApi.Model.Enums;
+using Newtonsoft.Json.Linq;
+
+namespace MergeOpenApi.Merge
+{
+    public interface IMergeOpenApiSchemas
+    {
+        void Execute(IList<ServiceDefinition> services);
+    }
+    
+    public class MergeOpenApiSchemas : IMergeOpenApiSchemas
+    {
+        private readonly IGetConfigurationCached _getConfigurationCached;
+        private readonly IMergeOpenApiSchema _mergeOpenApiSchema;
+        private readonly IInsertMergedSchema _insertMergedSchema;
+        private readonly IUpdateServiceStatus _updateServiceStatus;
+
+        public MergeOpenApiSchemas(IGetConfigurationCached getConfigurationCached,
+            IMergeOpenApiSchema mergeOpenApiSchema,
+            IInsertMergedSchema insertMergedSchema, 
+            IUpdateServiceStatus updateServiceStatus)
+        {
+            _getConfigurationCached = getConfigurationCached;
+            _mergeOpenApiSchema = mergeOpenApiSchema;
+            _insertMergedSchema = insertMergedSchema;
+            _updateServiceStatus = updateServiceStatus;
+        }
+        
+        public void Execute(IList<ServiceDefinition> services)
+        {
+            if (services.Count(x => x.Status == ServiceStatus.Fetched || x.Status == ServiceStatus.Done) == 1)
+            {
+                services.First().Status = ServiceStatus.Done;
+                using (var scope = new TransactionScope())
+                {
+                    _insertMergedSchema.Execute(services.First().JsonData, 1);
+                    _updateServiceStatus.Execute(services);
+                
+                    scope.Complete();     
+                }
+                return;
+            }
+            
+            var firstId = services.Min(x => x.Id);
+
+            var primary = services.First(x => x.Id == firstId);
+
+            var mainSchema = JObject.Parse(primary.JsonData);
+
+            var remaining = services.Where(x => x.Id != primary.Id).ToList();
+
+            remaining.ForEach(x => MergeServiceIntoMainSchema(mainSchema, x));
+
+            UpdateSchemaMasterData(mainSchema);
+
+            using (var scope = new TransactionScope())
+            {
+                _insertMergedSchema.Execute(mainSchema.ToString(), services.Count(x => x.Status == ServiceStatus.Done));
+
+                primary.Status = ServiceStatus.Done;
+                primary.Retry = 0;
+            
+                _updateServiceStatus.Execute(services);
+        
+                scope.Complete();
+            }
+        }
+        
+        private void MergeServiceIntoMainSchema(JObject mainSchema, ServiceDefinition service)
+        {
+            try
+            {
+                _mergeOpenApiSchema.Execute(mainSchema, service);
+
+                service.Status = ServiceStatus.Done;
+                service.Retry = 0;
+            }
+            catch (Exception e)
+            {
+                service.Retry++;
+            }
+        }
+        
+        private void UpdateSchemaMasterData(JObject mainSchema)
+        {
+            var configuration = _getConfigurationCached.Execute();
+
+            mainSchema["info"]["title"] = configuration.Title;
+            mainSchema["info"]["description"] = configuration.Description;
+
+            if (!string.IsNullOrEmpty(configuration.TermsUrl))
+            {
+                mainSchema["info"]["termsOfService"] = configuration.TermsUrl;
+            }
+            if (!string.IsNullOrEmpty(configuration.ContactEmail))
+            {
+                mainSchema["info"]["contact"] = new JObject();
+                mainSchema["info"]["contact"]["email"] = configuration.ContactEmail;
+            }
+            
+            if (!string.IsNullOrEmpty(configuration.ContactEmail))
+            {
+                mainSchema["info"]["license"] = new JObject();
+                mainSchema["info"]["license"]["name"] = configuration.LicenseName;
+                mainSchema["info"]["license"]["url"] = configuration.LicenseUrl;
+            }
+
+            if (configuration.SecurityType == SecurityType.BasicAuth)
+            {
+                mainSchema["components"]["securitySchemes"] = new JObject();
+                mainSchema["components"]["securitySchemes"]["basicAuth"] = new JObject();
+                mainSchema["components"]["securitySchemes"]["basicAuth"]["type"] = "http";
+                mainSchema["components"]["securitySchemes"]["basicAuth"]["scheme"] = "basic";
+            }
+
+            if (configuration.SecurityType == SecurityType.ApiKey)
+            {
+                mainSchema["components"]["securitySchemes"] = new JObject();
+                mainSchema["components"]["securitySchemes"]["ApiKeyAuth"] = new JObject();
+                mainSchema["components"]["securitySchemes"]["ApiKeyAuth"]["type"] = "apiKey";
+                mainSchema["components"]["securitySchemes"]["ApiKeyAuth"]["in"] = "header";
+                mainSchema["components"]["securitySchemes"]["ApiKeyAuth"]["name"] = configuration.SecurityKeyName;
+            }
+        }
+    }
+}
