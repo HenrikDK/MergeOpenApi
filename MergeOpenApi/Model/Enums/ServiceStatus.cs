ecbac40e93f91d73dbc6bd14fa95e43399d48be2
HenrikDK <commander.john.crichton@gmail.com>
2020-01-27 15:19:15 +0100

    add types and enums to batch service

---
+++ MergeOpenApi/Model/Enums/ServiceStatus.cs
@@ -0,0 +1,11 @@
+namespace MergeOpenApi.Model.Enums
+{
+    public enum ServiceStatus
+    {
+        Deployed = 0,
+        Fetched = 1,
+        Processing = 2,
+        Done = 3,
+        Disabled = 4
+    }
+}
---
+++ MergeOpenApi/Model/ServiceDefinition.cs
@@ -0,0 +1,13 @@
+using MergeOpenApi.Model.Enums;
+
+namespace MergeOpenApi.Model
+{
+    public class ServiceDefinition
+    {
+        public int Id { get; set; }
+        public string JsonData { get; set; }
+        public ServiceStatus Status { get; set; }
+        public int Retry { get; set; }
+        public string ServiceUrls { get; set; }
+    }
+}
