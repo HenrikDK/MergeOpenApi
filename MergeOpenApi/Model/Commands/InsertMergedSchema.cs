03c6870a36b85cc76d5fa51ad58a4c5b9014cbd0
HenrikDK <HenrikDK@some.place>
2021-11-18 21:49:49 +0100

    cleanup in worker

--- MergeOpenApi/Model/Commands/InsertMergedSchema.cs
+++ MergeOpenApi/Model/Commands/InsertMergedSchema.cs
@@ -1,30 +1,28 @@
-using Dapper;
 using MergeOpenApi.Infrastructure;
 
-namespace MergeOpenApi.Model.Commands
-{
-    public interface IInsertMergedSchema
-    {
-        void Execute(string json, int serviceCount);
-    }
-    
-    public class InsertMergedSchema : IInsertMergedSchema
-    {
-        private readonly IConnectionFactory _connectionFactory;
-        
-        public InsertMergedSchema(IConnectionFactory connectionFactory)
-        {
-            _connectionFactory = connectionFactory;
-        }
+namespace MergeOpenApi.Model.Commands;
 
-        public void Execute(string json, int serviceCount)
-        {
-            var sql = @" /* MergeOpenApi */
+public interface IInsertMergedSchema
+{
+    void Execute(string json, int serviceCount);
+}
+    
+public class InsertMergedSchema : IInsertMergedSchema
+{
+    private readonly IConnectionFactory _connectionFactory;
+        
+    public InsertMergedSchema(IConnectionFactory connectionFactory)
+    {
+        _connectionFactory = connectionFactory;
+    }
+
+    public void Execute(string json, int serviceCount)
+    {
+        var sql = @" /* MergeOpenApi */
 insert into openapi.schema (jsondata, services, created, createdby)
 values (@json, @serviceCount, current_timestamp, 'MergeOpenApi');";
 
-            using var connection = _connectionFactory.Get();
-            connection.Execute(sql, new { json, serviceCount });
-        }
+        using var connection = _connectionFactory.Get();
+        connection.Execute(sql, new { json, serviceCount });
     }
 }
--- MergeOpenApi/Model/ServiceDefinition.cs
+++ MergeOpenApi/Model/ServiceDefinition.cs
@@ -1,13 +1,12 @@
 using MergeOpenApi.Model.Enums;
 
-namespace MergeOpenApi.Model
+namespace MergeOpenApi.Model;
+
+public class ServiceDefinition
 {
-    public class ServiceDefinition
-    {
-        public int Id { get; set; }
-        public string JsonData { get; set; }
-        public ServiceStatus Status { get; set; }
-        public int Retry { get; set; }
-        public string ServiceUrls { get; set; }
-    }
+    public int Id { get; set; }
+    public string JsonData { get; set; }
+    public ServiceStatus Status { get; set; }
+    public int Retry { get; set; }
+    public string ServiceUrls { get; set; }
 }
