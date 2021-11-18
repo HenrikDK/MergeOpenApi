3e15e04f4eb2d20359cfa6178086f636f672253c
HenrikDK <commander.john.crichton@gmail.com>
2021-11-18 21:50:58 +0100

    cleanup in ui

--- MergeOpenApi.Ui/Startup.cs
+++ MergeOpenApi.Ui/Startup.cs
@@ -1,66 +1,56 @@
-using Lamar.Microsoft.DependencyInjection;
 using MergeOpenApi.Ui.Infrastructure;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Prometheus;
-using Swashbuckle.AspNetCore.SwaggerUI;
 
-namespace MergeOpenApi.Ui
+namespace MergeOpenApi.Ui;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddLamar(new ApiRegistry());
+        services.AddMemoryCache();
+
+        if (Program.Debug)
         {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-            services.AddLamar(new ApiRegistry());
-            services.AddMemoryCache();
-
-            if (Program.Debug)
+            services.AddLogging(x =>
             {
-                services.AddLogging(x =>
-                {
-                    x.AddDebug();
-                    x.AddConsole();
-                });
-            }
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (Program.Debug || env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseSwaggerUI(x =>
-            {
-                x.RoutePrefix = "";
-                x.SwaggerEndpoint("/swagger.json", "MergeOpenApi Api v1");
-                x.ConfigObject.AdditionalItems["tagsSorter"] = "alpha";
-                x.ConfigObject.DefaultModelsExpandDepth = -1;
-                x.DocExpansion(DocExpansion.None);
-            });
-
-            app.UseRouting();
-            app.UseHttpMetrics();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapMetrics();
+                x.AddDebug();
+                x.AddConsole();
             });
         }
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (Program.Debug || env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseSwaggerUI(x =>
+        {
+            x.RoutePrefix = "";
+            x.SwaggerEndpoint("/swagger.json", "MergeOpenApi Api v1");
+            x.ConfigObject.AdditionalItems["tagsSorter"] = "alpha";
+            x.ConfigObject.DefaultModelsExpandDepth = -1;
+            x.DocExpansion(DocExpansion.None);
+        });
+
+        app.UseRouting();
+        app.UseHttpMetrics();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapMetrics();
+        });
     }
 }
