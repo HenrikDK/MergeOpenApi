655384c3cdc856c0d673446d17c186c491cff179
HenrikDK <HenrikDK@some.place>
2021-11-18 21:49:13 +0100

    add global usings

---
+++ MergeOpenApi/GlobalUsings.cs
@@ -0,0 +1,23 @@
+global using System;
+global using System.Data;
+global using System.Diagnostics;
+global using System.Linq;
+global using System.Collections.Generic;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System.Transactions;
+
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+
+global using Dapper;
+global using Flurl;
+global using Flurl.Http;
+global using Flurl.Util;
+global using Lamar;
+global using Lamar.Microsoft.DependencyInjection;
+global using MoreLinq.Extensions;
+global using Newtonsoft.Json.Linq;
+global using Prometheus;
