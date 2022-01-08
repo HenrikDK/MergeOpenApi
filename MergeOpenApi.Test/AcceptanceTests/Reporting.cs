37b46011c0c34162c462388667e86475f87c9d57
HenrikDK <commander.john.crichton@gmail.com>
2022-01-08 21:07:09 +0100

    fix acceptance test reporting configuration

--- MergeOpenApi.Test/AcceptanceTests/Reporting.cs
+++ MergeOpenApi.Test/AcceptanceTests/Reporting.cs
@@ -8,8 +8,8 @@ public class Reporting
         Configurator.BatchProcessors.HtmlReport.Disable();
 
         var report = new DefaultHtmlReportConfiguration();
-        report.OutputFileName = typeof(Program).Namespace + ".AcceptanceTests.html";
-        report.ReportHeader = typeof(Program).Namespace + " acceptance tests";
+        report.OutputFileName = typeof(ServiceHost).Namespace + ".AcceptanceTests.html";
+        report.ReportHeader = typeof(ServiceHost).Namespace + " acceptance tests";
         report.ReportDescription = "Automated tests of business critical functionality";
 
         Configurator.BatchProcessors.Add(new HtmlReporter(report));
