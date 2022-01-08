c3c19dd65dc097698c4b0c6aac8856b445be922f
HenrikDK <commander.john.crichton@gmail.com>
2022-01-08 20:54:55 +0100

    cleanup global usings in ui

--- MergeOpenApi.Ui/GlobalUsings.cs
+++ MergeOpenApi.Ui/GlobalUsings.cs
@@ -1,16 +1,13 @@
 global using System;
 global using System.Data;
-global using System.Diagnostics;
 global using System.Linq;
 
-global using Microsoft.AspNetCore;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.Extensions.Caching.Memory;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Logging;
 
 global using Dapper;
@@ -18,3 +15,5 @@ global using Lamar;
 global using Lamar.Microsoft.DependencyInjection;
 global using Prometheus;
 global using Swashbuckle.AspNetCore.SwaggerUI;
+global using Serilog;
+global using Serilog.Formatting.Json;
