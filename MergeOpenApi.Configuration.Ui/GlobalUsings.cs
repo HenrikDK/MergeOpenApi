89e527aeca31872596a5e58e0bfd19973f579635
HenrikDK <commander.john.crichton@gmail.com>
2022-01-08 21:16:06 +0100

    add to global usings

--- MergeOpenApi.Configuration.Ui/GlobalUsings.cs
+++ MergeOpenApi.Configuration.Ui/GlobalUsings.cs
@@ -5,16 +5,16 @@ global using System.Linq;
 global using System.Collections.Generic;
 global using System.ComponentModel.DataAnnotations;
 
-global using Microsoft.AspNetCore;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Logging;
 
 global using Dapper;
 global using Lamar;
 global using Lamar.Microsoft.DependencyInjection;
 global using Prometheus;
+global using Serilog;
+global using Serilog.Formatting.Json;
