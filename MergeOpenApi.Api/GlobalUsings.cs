3218fedde43872ea624b943e4d3066e821091901
HenrikDK <commander.john.crichton@gmail.com>
2022-01-08 20:23:28 +0100

    update global usings

--- MergeOpenApi.Api/GlobalUsings.cs
+++ MergeOpenApi.Api/GlobalUsings.cs
@@ -1,18 +1,14 @@
 global using System;
 global using System.Data;
-global using System.Diagnostics;
-global using System.Linq;
 global using System.Collections.Generic;
 global using System.IO;
 global using System.Reflection;
 
-global using Microsoft.AspNetCore;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.OpenApi.Models;
 
@@ -20,3 +16,5 @@ global using Dapper;
 global using Lamar;
 global using Lamar.Microsoft.DependencyInjection;
 global using Prometheus;
+global using Serilog;
+global using Serilog.Formatting.Json;
